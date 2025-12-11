8e7cf3282b906404c6ea846544c3998b33b79e64
ADKAR DNYANESH RAMDAS <dnyaneshadkar@gmail.com>
2025-12-11 19:07:52 +0530

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,19 @@
+
+public class Datatypes {
+	public static void main(String args[]) {
+		byte a = 25;
+		short b = 479;
+		int c = 46;
+		float d= 45.9f;
+		double e = 4343.545d;
+		long f = 345345l;
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+	}
+
+}
